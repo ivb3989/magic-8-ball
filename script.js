e7fd28da429383a0012fc730e5c17cf375760036
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2021-12-08 17:39:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.body.innerHTML = `
       </div>
     </div>
   </div>
-  <a href="https://ivb3989.github.io/-Wn/" class="back">PLAY</a>
+  <a href="https://ivb3989.github.io/-Wn/" class="back">Back</a>
 `;
 let options = [
   "It is certain", "Yes",
